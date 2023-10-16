41bb1fd38d8b2669a67adfebfc9006212b4a7b01
a-gorman <gormanalex1@gmail.com>
2023-10-16 00:41:38 -0700

    Make the lightning work!!

--- NeonShooter.Core/Game/Graphics/Effect/Debug.cs
+++ NeonShooter.Core/Game/Graphics/Effect/Debug.cs
@@ -31,10 +31,38 @@ public class VectorEffect : IEffect
         if (!_timer.IsExpired)
         {
             var pointTexture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
-            pointTexture.SetData(new[] { Color.Red });
+            pointTexture.SetData(new[] { Color.White });
             
             spriteBatch.DrawLine(_start, _end, _color);
         }
     }
-    
+
+    public class PointEffect : IEffect {
+        private readonly Vector2 _position;
+        private readonly Color _color;
+
+        private readonly GameTimer _timer;
+
+        public bool IsExpired => _timer.IsExpired;
+
+        public PointEffect(Vector2 position, Color color, int duration = 1) {
+            _position = position;
+            _color = color;
+            _timer = GameTimer.FromFrames(duration);
+        }
+
+        public void Update() {
+            _timer.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            if (!_timer.IsExpired) {
+                var pointTexture = new Texture2D(spriteBatch.GraphicsDevice, 3, 3);
+                pointTexture.SetData(new[] { Color.White });
+
+                spriteBatch.Draw(pointTexture, _position, null, _color);
+            }
+        }
+    }
+
 }
