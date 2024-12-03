4bf127a5594f0d5d7c192abe119d8ddd380b9a9f
a-gorman <gormanalex1@gmail.com>
2024-12-02 22:03:49 -0800

    Add detection for winning the game

--- WarlockGame.Core/Game/Sim/Rule/MaxLives.cs
+++ WarlockGame.Core/Game/Sim/Rule/MaxLives.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using MonoGame.Extended;
 using WarlockGame.Core.Game.Effect;
 using WarlockGame.Core.Game.Entity;
+using WarlockGame.Core.Game.Log;
+using Debug = WarlockGame.Core.Game.Util.Debug;
 
 namespace WarlockGame.Core.Game.Sim.Rule;
 
@@ -20,11 +23,22 @@ class MaxLives {
     public void OnWarlockDestroyed(Warlock warlock) {
         int playerId = warlock.PlayerId;
         PlayerLives[playerId] -= 1;
+
         if (PlayerLives[playerId] != 0) {
             EffectManager.AddDelayedEffect(() => {
-                var respawnPosition = Sim.Simulation.ArenaSize / 2 + new Vector2(250, 0).Rotate(Sim.Simulation.Instance.Random.NextAngle());
+                var respawnPosition = Simulation.ArenaSize / 2 + new Vector2(250, 0).Rotate(Simulation.Instance.Random.NextAngle());
                 EntityManager.Add(new Warlock(playerId) { Position = respawnPosition });
             }, GameTimer.FromSeconds(5));
+        }
+        
+        if (PlayerLives.Count(x => x.Value > 0) == 1) {
+            var message = PlayerLives.First(x => x.Value > 0).Key == PlayerManager.LocalPlayer.Id
+                ? "You Win!!"
+                : "Another player has won the game!";
+            Debug.Visualize(message, Simulation.ArenaSize / 2, 100);
+        }
+        else if (PlayerLives.All(x => x.Value == 0)) {
+            Debug.Visualize("It's a draw!", Simulation.ArenaSize / 2, 100);
         }
     }
 }
