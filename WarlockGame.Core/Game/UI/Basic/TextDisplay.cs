531af2f25b87c8e517bef54e3307e9213fdefa10
Alex Gorman <gormanalex1@gmail.com>
2025-07-05 00:07:33 +0200

    Use dirty flag

--- WarlockGame.Core/Game/UI/Basic/TextDisplay.cs
+++ WarlockGame.Core/Game/UI/Basic/TextDisplay.cs
@@ -12,7 +12,7 @@ class TextDisplay : InterfaceComponent {
         get;
         set {
             field = value;
-            RecalculateWrappedText();
+            _isDirty = true;
         }
     }
 
@@ -20,12 +20,11 @@ class TextDisplay : InterfaceComponent {
         get => _text;
         set {
             _text = value;
-            RecalculateWrappedText();
+            _isDirty = true;
         }
     }
-    private string _text = string.Empty;
-    private string _wrappedText = string.Empty;
 
+    public Color TextColor { get; set; } = Color.White;
     public float TextScale { get; set; } = 1f;
 
     public SpriteFont Font {
@@ -33,20 +32,31 @@ class TextDisplay : InterfaceComponent {
         set {
             field = value;
             _fontHeight = value.MeasureString(" ").Y;
-            RecalculateWrappedText();
+            _isDirty = true;
         }
     }
 
     private float _fontHeight;
-
-    public Color TextColor { get; set; } = Color.White;
+    private string _text = string.Empty;
+    private string _wrappedText = string.Empty;
+    private bool _isDirty = false;
 
     public TextDisplay(SpriteFont? font = null) {
         Font = font ?? Art.Font;
     }
     
     public override void Draw(SpriteBatch spriteBatch) {
-        spriteBatch.DrawString(Art.Font, _wrappedText, Bounds.Location.ToVector2(), TextColor, scale: TextScale, rotation: 0, origin: Vector2.Zero, effects: SpriteEffects.None, layerDepth: 0);
+        if(_isDirty) {
+            RecalculateWrappedText();
+            _isDirty = false;
+        }
+
+        spriteBatch.DrawString(Art.Font, _wrappedText, Bounds.Location.ToVector2(), TextColor, 
+        scale: TextScale, 
+        rotation: 0, 
+        origin: Vector2.Zero, 
+        effects: SpriteEffects.None, 
+        layerDepth: 0);
     }
 
     private void RecalculateWrappedText() {
